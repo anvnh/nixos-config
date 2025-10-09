f61380e497a63cd70332e99f2e444c1619357f85
anvnh <anvo20052@gmail.com>
2025-10-09 17:33:13 +0700

    update capslock to ctrl

--- configuration.nix
+++ configuration.nix
@@ -61,18 +61,18 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
+  # Configure keymap
+  services.xserver = {
+    # Enable the X11 windowing system.
+    enable = true;
+
     layout = "us";
-    variant = "";
+
+    xkbOptions = "ctrl:nocaps";
   };
 
   # Enable CUPS to print documents.
